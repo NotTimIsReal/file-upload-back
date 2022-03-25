74250036fa6d1ea6dfe202101abc45a0af097d81
NotTimIsReal <alastairrintumathew@gmail.com>
2022-03-25 17:08:00 +1100

    using express types

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import * as passport from 'passport';
 import * as redis from 'redis';
 import * as connectRedis from 'connect-redis';
 import * as compression from 'compression';
+import { NestExpressApplication } from '@nestjs/platform-express';
 config();
 async function bootstrap() {
   const db = `redis://${process.env.REDISHOST}:${process.env.REDISPORT}`;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const redisStore = connectRedis(session);
   const client = redis.createClient({
     url: db,
@@ -26,6 +27,7 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(compression());
+  app.set('trust proxy', 1);
   app.use(
     session({
       secret: process.env.SECRET,
